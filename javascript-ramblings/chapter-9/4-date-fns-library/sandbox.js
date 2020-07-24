54d6fc25db461b13fbb95bfe7d7da2f1bd8a513f
Remi Ogunsanya <agemostudios@gmail.com>
2020-07-24 11:29:58 +0100

    update file dateFns added

--- javascript-ramblings/chapter-9/4-date-fns-library/sandbox.js
+++ javascript-ramblings/chapter-9/4-date-fns-library/sandbox.js
@@ -11,7 +11,10 @@ console.log(dateFns.format(now,'YYYY')); // integer for the year
 console.log(dateFns.format(now,'MMMM')); // returns a string for the month - March
 console.log(dateFns.format(now,'MMM')); // returns a shorten string for the month - Mar
 console.log(dateFns.format(now,'dddd')); // returns a string for the day- Friday
-console.log(dateFns.format(now,'ddd')); // returns a shorten string for the day- Fri
- 
+console.log(dateFns.format(now,'ddd')); // returns a shorten string for the day- Fri 
 
+// comparing dates using dateFns
+
+const before = new Date('February 1 2019 12:00:00');
+console.log(dateFns.distanceInWords(now,before,{addSuffix:true}));
 
