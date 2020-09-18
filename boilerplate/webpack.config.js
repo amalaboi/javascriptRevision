408ec40c8794a466919ae0cc180460d48a5d88b9
Remi Ogunsanya <agemostudios@gmail.com>
2020-09-18 14:39:07 +0100

    new files added

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -1,22 +1,20 @@
-//requre the path module from nodejs
 const path = require('path');
 
 module.exports = {
-
-  entry: './src/index.js', 
+  entry: './src/index.js',
   output: {
-        path: path.resolve(__dirname,'dist/assets'),
-        filename:'bundle.js'
-  },// end of output property
-  devServer:{
-    contentBase: path.resolve(__dirname,'dist'),
+    path: path.resolve(__dirname, 'dist/assets'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
     publicPath: '/assets/'
   },
   module: {
     rules: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      use:{
+      use: {
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-env']
@@ -24,5 +22,4 @@ module.exports = {
       }
     }]
   }
-
-};// end of module.exports
+};
